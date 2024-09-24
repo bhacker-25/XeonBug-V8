850a8932e15de45b5a70f642eba6571150349053
bhacker-25 <indianofficialacverification@yahoo.com>
2024-09-24 20:42:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,26 +10,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
-global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.ownernomer = "919401105736"
+global.ownername = "BHACKER"
+global.ytname = "YT: BHACKER"
+global.socialm = "GitHub: BHACKER"
+global.location = "INDIA, ASSAM"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '919401105736'  //creator number
+global.ownername = 'BHACKER' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'Bhacker ❤️\n\nContact: +919401105736'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://chat.whatsapp.com/HAGxgim2IfIALNxYhSiKa1'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
